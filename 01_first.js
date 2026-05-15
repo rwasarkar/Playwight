f9b889964171d849310fded57ecfdc4092209a57
rwasarkar <rwasarkar@gmail.com>
2026-05-15 21:19:41 +0530

    Update JavaScript examples and restructure files

---
+++ 01_first.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+let a = 5;
+console.log("Value of a",a);
+
+for (let a=0;a<=100;a++)
+{
+    console.log(a)
+    print();  
+}
+function print()
+{
+    console.log("for loop")
+}
---
+++ 02_for_loop.js
@@ -0,0 +1,12 @@
+let a = 5;
+console.log("Value of a",a);
+
+for (let a=0;a<=100;a++)
+{
+    console.log(a)
+    print();  
+}
+function print()
+{
+    console.log("for loop")
+}
