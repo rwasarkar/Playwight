69041def44f192250d85c911092157d2f1bbae58
rwasarkar <rwasarkar@gmail.com>
2026-05-16 13:45:33 +0530

    Added indetifier ex 1

---
+++ 06_identifier_1.js
@@ -0,0 +1,13 @@
+var a =10
+var _a =10
+var a9 =35
+var $a =10
+var Ram = 20
+var ram = 30
+var fun = 40
+console.log(" values:",a,a9,_a,$a,Ram,ram,fun)
+
+// var ram w will not work with space
+// var a&b will not work in special char
+// var 123 will not work as can not start with numbers
+
